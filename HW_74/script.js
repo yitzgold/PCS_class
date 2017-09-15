e340aa002b49f0923f11f3a096d13a3ca6ee0708
yitzgold <yitzigoldman@gmail.com>
2017-09-14 22:07:40 -0400

    HW_74

---
+++ HW_74/script.js
@@ -0,0 +1,10 @@
+alert(name);
+
+function greaterThan(a, b){
+    return a > b;
+}
+
+console.log(greaterThan(10, 9));
+console.log(greaterThan(10, 10));
+console.log(greaterThan(8, 9));
+console.log(greaterThan(-20, -39)); 
