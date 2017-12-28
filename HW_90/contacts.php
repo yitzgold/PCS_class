5273a96561c687bc24306ba0a744a8082deeb838
yitzgold <yitzigoldman@gmail.com>
2017-12-27 19:03:40 -0500

    HW_90

---
+++ HW_90/contacts.php
@@ -0,0 +1,11 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    try {
+        $db = new PDO("mysql:host=localhost;dbname=test", "me", "password", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+        $contacts = $db->query('SELECT firstName, lastName, email, phone FROM contacts')->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($contacts);  
+    }catch(PDOException $e) {
+        die("Something is wrong with your connection " . $e->getMessage()); 
+    }
+
+?>
